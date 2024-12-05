229cf605eeb536da471b7bf11167664f0e6ace55
Will Delorm <willdelorm@gmail.com>
2024-12-04 20:37:30 -0800

    save part 2 in progress

--- 04/solution.js
+++ 04/solution.js
@@ -11,7 +11,7 @@ const ADJACENT_SPACES = [
   [1, 1],
 ];
 
-function searchWords(fp, str) {
+function searchWords(fp, str, p2 = false) {
   function nextStep(pos, dir, slicedArr) {
     const [nextX, nextY] = [pos[0] + dir[0], pos[1] + dir[1]];
     // console.log([nextX, nextY]);
@@ -62,19 +62,36 @@ function searchWords(fp, str) {
 
   // Validate each X
   let foundWords = 0;
+  let posFoundAs = {};
   firstLetters.forEach((pos) => {
     ADJACENT_SPACES.forEach((dir) => {
       // console.log("checking", pos, "toward", dir);
       if (nextStep(pos, dir, strArr.slice(1))) {
         foundWords++;
+        // console.log(data[pos[1] + dir[1]][pos[0] + dir[0]]);
+        if (p2) {
+          const posA = [pos[0] + dir[0], pos[1] + dir[1]];
+          // console.log(posA);
+          if (posFoundAs[posA]) posFoundAs[posA]++;
+          else posFoundAs[posA] = 1;
+        }
       }
     });
   });
 
-  console.log(foundWords);
+  console.log(str, foundWords);
+  if (p2) {
+    // console.log(posFoundAs);
+    const numX_MAS = Object.keys(posFoundAs)
+      .sort()
+      .reduce((count, pos) => {
+        return posFoundAs[pos] === 2 ? count + 1 : count;
+      }, 0);
+    console.log("X-MAS", numX_MAS);
+  }
 }
 
-searchWords("test.txt", "XMAS");
-searchWords("input.txt", "XMAS");
-searchWords("test.txt", "MAS");
-searchWords("input.txt", "MAS");
+// searchWords("test.txt", "XMAS");
+// searchWords("input.txt", "XMAS");
+searchWords("test2.txt", "MAS", true);
+searchWords("input.txt", "MAS", true);
