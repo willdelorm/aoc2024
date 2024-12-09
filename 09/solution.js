3a2ddf23f95fde4d5364f505d42fb348ff74a94b
Will Delorm <willdelorm@gmail.com>
2024-12-09 14:05:31 -0800

    Setup day 9

---
+++ 09/solution.js
@@ -0,0 +1,10 @@
+import { getInputData } from "../utils/utils.js";
+
+function main(fp) {
+  // Convert dense-format data
+  // Condense data
+  // Calculate filesystem checksum
+}
+
+console.log("test", main("test.txt"));
+// console.log("input", main("input.txt"));
