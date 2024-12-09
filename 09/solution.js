a4a0cd49bc596631f3ab7881b2a27588711f68f0
Will Delorm <willdelorm@gmail.com>
2024-12-09 14:30:48 -0800

    start part 1

--- 09/solution.js
+++ 09/solution.js
@@ -1,6 +1,14 @@
 import { getInputData } from "../utils/utils.js";
+import { Node, DoublyLinkedList } from "../utils/linkedList.js";
 
 function main(fp) {
+  const data = getInputData(fp).split("");
+
+  const fileSystem = new DoublyLinkedList();
+  let id = 0;
+  data.forEach((len, i) => {
+    for (let j = 0; j < Number(len); j++) {}
+  });
   // Convert dense-format data
   // Condense data
   // Calculate filesystem checksum
---
+++ utils/linkedList.js
@@ -0,0 +1,42 @@
+export class Node {
+  constructor(val) {
+    this.data = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+export class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  isEmpty() {
+    if (this.head == null) return true;
+    return false;
+  }
+
+  addItem(val) {
+    let temp = new Node(val);
+
+    if (this.head == null) {
+      this.head = temp;
+      this.tail = temp;
+    } else {
+      temp.prev = this.tail;
+      this.tail.next = temp;
+      this.tail = this.tail.next;
+    }
+  }
+
+  display() {
+    if (!this.isEmpty()) {
+      let curr = this.head;
+      while (curr !== null) {
+        console.log(curr.data);
+        curr = curr.next;
+      }
+    }
+  }
+}
