e02b9321e7801a6f814efe55ce71d521d828dfac
Will Delorm <willdelorm@gmail.com>
2024-12-10 12:55:24 -0800

    revert to part 1 solution

--- 04/solution.js
+++ 04/solution.js
@@ -1,5 +1,12 @@
+/**
+ *  ===========================
+ *  === Day 4: Ceres Search ===
+ *  ===========================
+ */
+
 import { getInputData } from "../utils/utils.js";
 
+//* Part 1
 const ADJACENT_SPACES = [
   [-1, -1],
   [-1, 0],
@@ -11,42 +18,13 @@ const ADJACENT_SPACES = [
   [1, 1],
 ];
 
-function searchWords(fp, str, p2 = false) {
-  function nextStep(pos, dir, slicedArr) {
-    const [nextX, nextY] = [pos[0] + dir[0], pos[1] + dir[1]];
-    // console.log([nextX, nextY]);
-
-    // Is next pos valid?
-    if (nextX < 0 || nextX >= width || nextY < 0 || nextY >= height) {
-      // console.log("Off grid");
-      return 0;
-    }
-
-    // Is next pos the letter we want?
-    const tapePos = width * nextY + nextX;
-    if (dataTape[tapePos] !== slicedArr[0]) {
-      // console.log("Wrong letter", dataTape[[nextX, nextY].join("")]);
-      return 0;
-    }
-
-    // Is next pos the final letter?
-    if (slicedArr.length !== 1) {
-      // console.log("Keep going!");
-      return nextStep([nextX, nextY], dir, slicedArr.slice(1));
-    }
-    // console.log("We have a winner!");
-    return 1;
-  }
-
-  // console.log("--- Begin XMAS count ---");
-  const data = getInputData(fp).split("\n");
+function main(fp, word) {
+  const data = getInputData(fp).split("\r\n");
   const dataTape = data.join("");
-  const strArr = str.split("");
+  const wordChars = word.split("");
 
-  // Word Search dimensions
   const width = data[0].length;
   const height = data.length;
-  // console.log(`Grid dimensions:`, [width, height]);
 
   // Get position of all instances of first letter
   let firstLetters = [];
@@ -54,44 +32,41 @@ function searchWords(fp, str, p2 = false) {
     let x = i % width;
     let y = Math.floor(i / height);
 
-    if (char === strArr[0]) {
+    if (char === wordChars[0]) {
       firstLetters.push([x, y]);
     }
   });
-  // console.log("All X positions:\n", firstLetters);
+
+  function nextStep(pos, dir, slicedArr) {
+    const [nextX, nextY] = [pos[0] + dir[0], pos[1] + dir[1]];
+    // Is next pos valid?
+    if (nextX < 0 || nextX >= width || nextY < 0 || nextY >= height) {
+      return 0;
+    }
+    // Is next pos the letter we want?
+    const tapePos = width * nextY + nextX;
+    if (dataTape[tapePos] !== slicedArr[0]) {
+      return 0;
+    }
+    // Is next pos the final letter?
+    if (slicedArr.length !== 1) {
+      return nextStep([nextX, nextY], dir, slicedArr.slice(1));
+    }
+    return 1;
+  }
 
   // Validate each X
-  let foundWords = 0;
-  let posFoundAs = {};
+  let count = 0;
   firstLetters.forEach((pos) => {
     ADJACENT_SPACES.forEach((dir) => {
-      // console.log("checking", pos, "toward", dir);
-      if (nextStep(pos, dir, strArr.slice(1))) {
-        foundWords++;
-        // console.log(data[pos[1] + dir[1]][pos[0] + dir[0]]);
-        if (p2) {
-          const posA = [pos[0] + dir[0], pos[1] + dir[1]];
-          // console.log(posA);
-          if (posFoundAs[posA]) posFoundAs[posA]++;
-          else posFoundAs[posA] = 1;
-        }
+      if (nextStep(pos, dir, wordChars.slice(1))) {
+        count++;
       }
     });
   });
 
-  console.log(str, foundWords);
-  if (p2) {
-    // console.log(posFoundAs);
-    const numX_MAS = Object.keys(posFoundAs)
-      .sort()
-      .reduce((count, pos) => {
-        return posFoundAs[pos] === 2 ? count + 1 : count;
-      }, 0);
-    console.log("X-MAS", numX_MAS);
-  }
+  return count;
 }
 
-// searchWords("test.txt", "XMAS");
-// searchWords("input.txt", "XMAS");
-searchWords("test2.txt", "MAS", true);
-searchWords("input.txt", "MAS", true);
+console.log(main("test.txt", "XMAS")); // 18
+console.log(main("input.txt", "XMAS"));
