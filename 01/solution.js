528200f5b3a5c7f8f621022ed7491f0e90734e29
Will Delorm <willdelorm@gmail.com>
2024-12-10 14:14:59 -0800

    refactor

--- 01/solution.js
+++ 01/solution.js
@@ -9,60 +9,48 @@ import { getInputData } from "../utils/utils.js";
 //* Part 1
 function calculateTotalDistance(fp) {
   const data = getInputData(fp);
-  const { listLeft, listRight } = getLists(data);
+  const { list1, list2 } = getLists(data);
 
-  listLeft.sort();
-  listRight.sort();
-
-  let totalDistance = 0;
-  listLeft.forEach((locationId, index) => {
-    const leftId = Number(locationId),
-      rightId = Number(listRight[index]);
-    totalDistance += Math.abs(leftId - rightId);
-  });
-
-  return totalDistance;
+  return list1.reduce((sum, _, i) => {
+    return sum + Math.abs(list1[i] - list2[i]);
+  }, 0);
 }
 
 function getLists(data) {
   const locationIds = data.match(/[0-9]+/g);
-  const listLeft = [],
-    listRight = [];
+  const list1 = [],
+    list2 = [];
 
   locationIds.forEach((locationId, index) => {
     if (index % 2 === 0) {
-      listLeft.push(locationId);
+      list1.push(locationId);
     } else {
-      listRight.push(locationId);
+      list2.push(locationId);
     }
   });
 
-  return { listLeft, listRight };
+  list1.sort((a, b) => a - b);
+  list2.sort((a, b) => a - b);
+
+  return { list1, list2 };
 }
 
 //* Part 2
 function calculateSimilarityScore(fp) {
   const data = getInputData(fp);
-  const { listLeft, listRight } = getLists(data);
+  const { list1, list2 } = getLists(data);
 
-  const listMultipliers = {};
-  listRight.forEach((locationId) => {
-    if (listMultipliers[locationId]) {
-      listMultipliers[locationId]++;
-    } else {
-      listMultipliers[locationId] = 1;
-    }
+  const numCounts = {};
+  list2.forEach((locationId) => {
+    numCounts[locationId]
+      ? numCounts[locationId]++
+      : (numCounts[locationId] = 1);
   });
 
-  let totalSimilarityScore = 0;
-  listLeft.forEach((locationId) => {
-    const multiplier = listMultipliers[locationId]
-      ? listMultipliers[locationId]
-      : 0;
-    totalSimilarityScore += Number(locationId) * multiplier;
-  });
-
-  return totalSimilarityScore;
+  return list1.reduce((sum, id) => {
+    const multiplier = numCounts[id] ? numCounts[id] : 0;
+    return sum + Number(id) * multiplier;
+  }, 0);
 }
 
 console.log("Part 1:", calculateTotalDistance("input.txt"));
