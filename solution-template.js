a271654dfb0e7bd5e21da8d4ddc22342128500d0
Will Delorm <willdelorm@gmail.com>
2024-12-11 15:39:24 -0800

    create template file

---
+++ solution-template.js
@@ -0,0 +1,20 @@
+/**
+ *  =================================
+ *  === Day 1: Historian Hysteria ===
+ *  =================================
+ */
+
+import { getInputData } from "../utils/utils.js";
+
+//* Part 1
+function main(fp) {
+  const data = getInputData(fp);
+  console.log(data);
+}
+
+//* Part 2
+// function main(fp) {}
+
+console.log("Test:", main("test.txt"));
+// console.log("Part 1:", main("input.txt"));
+// console.log("Part 2:", main("input.txt"));
